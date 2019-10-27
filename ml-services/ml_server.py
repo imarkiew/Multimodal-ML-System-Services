db09dea47e24da457dcda7c815c3a6b92d1de154
Igor Markiewicz <igomar067@gmail.com>
2019-10-27 11:52:23 +0100

    Added config file for ml_server

--- ml-services/ml_server.py
+++ ml-services/ml_server.py
@@ -4,24 +4,25 @@ import requests
 import json
 import sqlalchemy as db
 import csv
+import configparser
 from sqlalchemy.orm import sessionmaker
 from argparse import ArgumentParser
 from examinations import Examinations
 
 
 parser = ArgumentParser()
-parser.add_argument("--service_address", help="Service IP address", type=str, required=True)
-parser.add_argument("--service_port", help="Service port", type=str, required=True)
-parser.add_argument("--db_url", help="MySQL database url", type=str, required=True)
-parser.add_argument("--ml_model_url", help="ML model url", type=str, required=True)
-parser.add_argument("--output_dir", help="Output directory for saving files", type=str, required=True)
-parser.add_argument("--label_class_matcher_file_path_for_skin_lesions", help="Label - class matcher file path for skin lesions", type=str, required=True)
+parser.add_argument("--config_file_path", help="Config file path", type=str, required=True)
+parser.add_argument("--config_type", help="Config type", type=str, required=True)
 args = parser.parse_args()
 arguments = args.__dict__
 
+config = configparser.ConfigParser()
+config.read(arguments["config_file_path"])
+config_file = config[arguments["config_type"]]
+
 app = Flask(__name__)
 
-engine = db.create_engine(arguments["db_url"])
+engine = db.create_engine(config_file["db_url"])
 Session = sessionmaker(bind=engine)
 session = Session()
 
@@ -33,17 +34,17 @@ def handle_skin_lesions_request():
 
         # discard metainfo from base64 encoded image
         content = body["content"].split(",")[1]
-        path = arguments["output_dir"] + body["fileName"]
+        path = config_file["output_dir"] + body["fileName"]
 
         with open(path, "wb") as f:
             f.write(base64.b64decode(content))
 
         data = json.dumps({"signature_name": "serving_default", "instances": [{"b64": content}]})
         headers = {"content-type": "application/json"}
-        json_response = requests.post(arguments["ml_model_url"], data=data, headers=headers)
+        json_response = requests.post(config_file["ml_model_url"], data=data, headers=headers)
         predictions = json.loads(json_response.text)
 
-        predictions = parse_json_with_predictions_for_skin_lesions(predictions, arguments["label_class_matcher_file_path_for_skin_lesions"], to_percent_and_round)
+        predictions = parse_json_with_predictions_for_skin_lesions(predictions, config_file["label_class_matcher_file_path_for_skin_lesions"], to_percent_and_round)
 
         print(predictions)
 
@@ -72,4 +73,4 @@ def to_percent_and_round(value, digits=2):
 
 
 if __name__ == "__main__":
-    app.run(host=arguments["service_address"], port=arguments["service_port"])
+    app.run(host=config_file["service_address"], port=config_file["service_port"])
