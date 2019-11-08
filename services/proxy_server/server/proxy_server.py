06cb1ad97a69fc405b71899f6c7f433088ed6a1b
Igor Markiewicz <igomar067@gmail.com>
2019-11-08 18:43:28 +0100

    Code's been refactored

---
+++ services/proxy_server/server/proxy_server.py
@@ -0,0 +1,88 @@
+"""
+.. module:: server
+   :synopsis: Proxy server between ML models, database and play server
+"""
+
+
+from flask import Flask, request, jsonify
+import json
+import sqlalchemy as db
+from sqlalchemy.orm import sessionmaker
+from argparse import ArgumentParser
+from examinations import Examinations
+from utilities import parse_predictions_for_skin_lesions, to_percent_and_round, \
+    preprocess_content_from_request_body, read_breast_cancer_file, get_response_fom_ml_service, parse_predictions_for_breast_cancer
+
+
+parser = ArgumentParser()
+parser.add_argument("--config_file_path", help="Config file path", type=str, required=True)
+parser.add_argument("--config_type", help="Config type", type=str, required=True)
+args = parser.parse_args()
+arguments = args.__dict__
+
+with open(arguments["config_file_path"], "r") as file:
+    config_file = json.load(file)[arguments["config_type"]]
+
+app = Flask(__name__)
+
+engine = db.create_engine(config_file["db_url"])
+Session = sessionmaker(bind=engine)
+session = Session()
+
+
+@app.route("/skinLesions", methods=["POST"])
+def handle_skin_lesions_request():
+        """Handle incoming HTTP POST request with json body for skin lesions
+
+           Returns:
+              HTTP response with 200 status if there wasn't any exception and 500 otherwise
+       """
+    # try:
+        body = request.json
+        content, path = preprocess_content_from_request_body(body, config_file["skin_lesions_output_dir"])
+        data = json.dumps({"signature_name": "serving_default", "instances": [{"b64": content}]})
+        json_response = get_response_fom_ml_service(data, config_file["skin_lesions_ml_model_url"])
+        predictions = parse_predictions_for_skin_lesions(json_response.text, config_file["skin_lesions_labels_classes_matcher"], to_percent_and_round)
+
+        session.add(Examinations(kind="skin-lesions", title=body["title"], date=body["date"],
+                                results=predictions, path=path, username=body["username"]))
+        session.commit()
+
+        response = jsonify(success=True)
+        response.status_code = 200
+        return response
+    # except:
+    #     response = jsonify(success=False)
+    #     response.status_code = 500
+    #     return response
+
+
+@app.route("/breastCancer", methods=["POST"])
+def handle_breast_cancer_request():
+        """Handle incoming HTTP POST request with json body for breast cancer
+
+           Returns:
+               HTTP response with 200 status if there wasn't any exception and 500 otherwise
+        """
+    # try:
+        body = request.json
+        _, path = preprocess_content_from_request_body(body, config_file["breast_cancer_output_dir"])
+        data = read_breast_cancer_file(path)
+        json_response = get_response_fom_ml_service(data, config_file["breast_cancer_ml_model_url"])
+        predictions = parse_predictions_for_breast_cancer(json_response.text, to_percent_and_round)
+
+        session.add(Examinations(kind="breast-cancer", title=body["title"], date=body["date"],
+                                results=predictions, path=path, username=body["username"]))
+        session.commit()
+
+        response = jsonify(success=True)
+        response.status_code = 200
+        return response
+    # except:
+    #     response = jsonify(success=False)
+    #     response.status_code = 500
+    #     return response
+
+
+if __name__ == "__main__":
+    app.run(host=config_file["service_address"], port=config_file["service_port"], threaded=True)
--- services/proxy_server/server/utilities.py
+++ services/proxy_server/server/utilities.py
@@ -5,9 +5,12 @@
 
 
 import json
+import base64
+import csv
+import requests
 
 
-def parse_json_with_predictions_for_skin_lesions(predictions, labels_classes_matcher, customize_printed_value=None):
+def parse_predictions_for_skin_lesions(predictions, labels_classes_matcher, customize_printed_value=None):
     """Parse predictions with renaming classes and custom printing of floats if necessary
 
     :param predictions:
@@ -15,11 +18,27 @@ def parse_json_with_predictions_for_skin_lesions(predictions, labels_classes_mat
     :param customize_printed_value:
     :return: json with parsed results
     """
+    predictions = json.loads(predictions)
     predictions = predictions["predictions"][0]
     if customize_printed_value is not None:
         return json.dumps({class_: customize_printed_value(predictions[int(label)]) for label, class_ in labels_classes_matcher.items()})
     else:
         return json.dumps({class_: predictions[int(label)] for label, class_ in labels_classes_matcher.items()})
+
+
+def parse_predictions_for_breast_cancer(predictions, customize_printed_value=None):
+    """Parse predictions with replacing "'" for '"'
+
+    :param predictions:
+    :param customize_printed_value:
+    :return:
+    """
+    predictions = json.loads(predictions)
+    print(predictions)
+    if customize_printed_value is not None:
+        return json.dumps({class_: customize_printed_value(prediction) for class_, prediction in predictions.items()}).replace("'", '"')
+    else:
+        return json.dumps(predictions).replace("'", '"')
 
 
 def to_percent_and_round(value, digits=2):
@@ -30,3 +49,62 @@ def to_percent_and_round(value, digits=2):
     :return: parsed and rounded value
     """
     return round(100 * value, digits)
+
+
+def get_content(body):
+    """ Get content from body and discard metainfo from base64 encoded json
+
+    :param body:
+    :return: parsed content
+    """
+    return body["content"].split(",")[1]
+
+
+def construct_save_file_path(body, base_file_path):
+    """ Concatenate string for save file path
+
+    :param body:
+    :param base_file_path:
+    :return: String for save file path
+    """
+    return base_file_path + body["username"] + "_" + body["fileName"]
+
+
+def preprocess_content_from_request_body(body, base_file_path="./"):
+    """Get content from request body and save it
+
+    :param body:
+    :param base_file_path:
+    :return: Json with content from request body and path to saved file
+    """
+    content = get_content(body)
+    path = construct_save_file_path(body, base_file_path)
+
+    with open(path, "wb") as f:
+        f.write(base64.b64decode(content))
+
+    return content, path
+
+
+def read_breast_cancer_file(path):
+    """Read saved breast cancer file
+
+    :param path:
+    :return: Json with data from breast cancer file
+    """
+    with open(path, "r") as file:
+        dict_reader = csv.DictReader(file, delimiter=",")
+        dict_raw = list(dict_reader)[0]
+        input_dict = {key: float(value) for key, value in dict_raw.items()}
+        return json.dumps(input_dict)
+
+
+def get_response_fom_ml_service(data, path):
+    """Get response from ml service
+
+    :param data:
+    :param path:
+    :return: Response from ml service
+    """
+    headers = {"content-type": "application/json"}
+    return requests.post(path, data=data, headers=headers)
